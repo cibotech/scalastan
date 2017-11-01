a5e452a7eb2b5344344aa46903cffb18559a2da7
Joe Wingbermuehle <joewing@joewing.net>
2017-11-01 14:35:34 -0500

    Add Horseshoe model.

--- src/main/scala/com/cibo/scalastan/models/LinearRegression.scala
+++ src/main/scala/com/cibo/scalastan/models/LinearRegression.scala
@@ -1,30 +1,21 @@
 package com.cibo.scalastan.models
 
-import com.cibo.scalastan.ScalaStan
+import com.cibo.scalastan._
 
-case class LinearRegression(xs: Seq[Double], ys: Seq[Double]) extends ScalaStan {
-
-  require(xs.length == ys.length, s"length of Xs (${xs.length}) does not equal the length of Ys (${ys.length})")
-
+case class LinearRegression() extends ScalaStan {
   private val n = data(int(lower = 0))
-  private val x = data(vector(n))
-  private val y = data(vector(n))
 
-  private val b = parameter(real())
-  private val m = parameter(real())
-  private val sig = parameter(real(lower = 0))
+  val x: StanDataDeclaration[StanVector] = data(vector(n))
+  val y: StanDataDeclaration[StanVector] = data(vector(n))
+
+  val b: StanParameterDeclaration[StanReal] = parameter(real())
+  val m: StanParameterDeclaration[StanReal] = parameter(real())
+  val sigma: StanParameterDeclaration[StanReal] = parameter(real(lower = 0))
 
   private val model = new Model {
-    sig ~ InvGamma(0.01, 0.01)
-    y ~ Normal(m * x + b, sig)
+    sigma ~ InvGamma(0.01, 0.01)
+    y ~ Normal(m * x + b, sigma)
   }
 
-  private lazy val results = model
-    .withData(x, xs)
-    .withData(y, ys)
-    .run()
-
-  def bhat: Double = results.mean(b)
-  def mhat: Double = results.mean(m)
-  def sigma: Double = results.mean(sig)
+  def compile: CompiledModel = model.compile
 }

8fc462bef53ebe8c18557a38f960b4c66e2c89c2
Joe Wingbermuehle <joewing@joewing.net>
2017-11-14 10:59:40 -0600

    Update LinearRegression model.

--- src/main/scala/com/cibo/scalastan/models/LinearRegression.scala
+++ src/main/scala/com/cibo/scalastan/models/LinearRegression.scala
@@ -2,20 +2,35 @@ package com.cibo.scalastan.models
 
 import com.cibo.scalastan._
 
-case class LinearRegression() extends ScalaStan {
-  private val n = data(int(lower = 0))
+case class LinearRegression(
+  xs: Seq[Seq[Double]],   // Inputs
+  ys: Seq[Double]         // Outputs
+) extends ScalaStan {
 
-  val x: StanDataDeclaration[StanVector] = data(vector(n))
-  val y: StanDataDeclaration[StanVector] = data(vector(n))
+  require(xs.length == ys.length, "Length of inputs must match the length of the outputs")
 
-  val b: StanParameterDeclaration[StanReal] = parameter(real())
-  val m: StanParameterDeclaration[StanReal] = parameter(real())
-  val sigma: StanParameterDeclaration[StanReal] = parameter(real(lower = 0))
+  private val n = data(int(lower = 0))  // Number of observations
+  private val p = data(int(lower = 0))  // Number of parameters
+  private val x: StanDataDeclaration[StanMatrix] = data(matrix(n, p))   // Inputs
+  private val y: StanDataDeclaration[StanVector] = data(vector(n))      // Outputs
+
+  val beta0: StanParameterDeclaration[StanReal] = parameter(real())             // Offset
+  val beta: StanParameterDeclaration[StanVector] = parameter(vector(p))         // Coefficients
+  val sigma: StanParameterDeclaration[StanReal] = parameter(real(lower = 0))    // Error
 
   private val model = new Model {
     sigma ~ InvGamma(0.01, 0.01)
-    y ~ Normal(m * x + b, sigma)
+    y ~ Normal(x * beta + beta0, sigma)
   }
 
   def compile: CompiledModel = model.compile
+    .withData(x, xs)
+    .withData(y, ys)
+
+  def predict(data: Seq[Double], results: StanResults): Double = {
+    val bestBeta0 = results.best(beta0)
+    val bestBeta = results.best(beta)
+    require(data.length == bestBeta.length)
+    bestBeta0 + data.zip(bestBeta).map { case (d, b) => d * b }.sum
+  }
 }

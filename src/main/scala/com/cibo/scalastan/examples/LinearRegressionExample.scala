723c292fb5b86b558896da953cd81c36e0163f52
Joe Wingbermuehle <joewing@joewing.net>
2017-11-14 10:59:40 -0600

    Update LinearRegression model.

--- src/main/scala/com/cibo/scalastan/examples/LinearRegressionExample.scala
+++ src/main/scala/com/cibo/scalastan/examples/LinearRegressionExample.scala
@@ -4,17 +4,16 @@ import com.cibo.scalastan.models.LinearRegression
 
 object LinearRegressionExample extends App {
 
-  val xs = Seq[Double](1, 2, 3, 4, 6)
+  val xs = Seq[Seq[Double]](Seq(1), Seq(2), Seq(3), Seq(4), Seq(6))
   val ys = Seq[Double](2.9, 6.1, 7.0, 9.2, 13.1)
 
-  val model = LinearRegression()
-  val results = model.compile
-    .withData(model.x, xs)
-    .withData(model.y, ys)
-    .run(chains = 4)
+  val model = LinearRegression(xs, ys)
+  val results = model.compile.run(chains = 4)
 
-  println(s"m:     ${results.best(model.m)}")
-  println(s"b:     ${results.best(model.b)}")
+  results.summary(System.out)
+
+  println(s"beta0: ${results.best(model.beta0)}")
+  println(s"beta:  ${results.best(model.beta)}")
   println(s"sigma: ${results.best(model.sigma)}")
 
 }

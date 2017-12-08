26468015163e6485ea488a7775abaeef8e4dc389
Joe Wingbermuehle <joewing@joewing.net>
2017-12-08 08:35:15 -0500

    Add parameter filtering to summary.

--- src/test/scala/com/cibo/scalastan/StanResultsSpec.scala
+++ src/test/scala/com/cibo/scalastan/StanResultsSpec.scala
@@ -4,55 +4,57 @@ import java.io._
 
 class StanResultsSpec extends ScalaStanBaseSpec {
 
-  private implicit val ss = new ScalaStan {}
-  private val model = CmdStanCompiledModel(new File("."), ss)
+  private object TestScalaStan extends ScalaStan {
+    val n = data(int())
+    val v1 = parameter(real())
+    val v2 = parameter(vector(n))
+    val v3 = parameter(vector(n)(n))
+  }
 
-  private val n = StanDataDeclaration[StanInt](StanInt())
-  private val v1 = StanParameterDeclaration[StanInt](StanInt())
-  private val v2 = StanParameterDeclaration[StanVector](StanVector(n))
-  private val v3 = StanParameterDeclaration[StanArray[StanVector]](StanArray(n, StanVector(n)))
+  private val model = CmdStanCompiledModel(new File("."), TestScalaStan)
+
 
   private val testData1 = Map[String, Int](
     "lp__" -> 1,
-    v1.emit -> 1,
-    s"${v2.emit}.1" -> 2,
-    s"${v2.emit}.2" -> 3,
-    s"${v2.emit}.3" -> 4,
-    s"${v3.emit}.1.1" -> 311,
-    s"${v3.emit}.1.2" -> 312,
-    s"${v3.emit}.2.1" -> 321,
-    s"${v3.emit}.2.2" -> 322,
-    s"${v3.emit}.3.1" -> 331,
-    s"${v3.emit}.3.2" -> 332
+    TestScalaStan.v1.emit -> 1,
+    s"${TestScalaStan.v2.emit}.1" -> 2,
+    s"${TestScalaStan.v2.emit}.2" -> 3,
+    s"${TestScalaStan.v2.emit}.3" -> 4,
+    s"${TestScalaStan.v3.emit}.1.1" -> 311,
+    s"${TestScalaStan.v3.emit}.1.2" -> 312,
+    s"${TestScalaStan.v3.emit}.2.1" -> 321,
+    s"${TestScalaStan.v3.emit}.2.2" -> 322,
+    s"${TestScalaStan.v3.emit}.3.1" -> 331,
+    s"${TestScalaStan.v3.emit}.3.2" -> 332
   ).mapValues(_.toString)
 
   private val testData2 = Map[String, Int](
     "lp__" -> 2,
-    v1.emit -> 0,
-    s"${v2.emit}.1" -> 1,
-    s"${v2.emit}.2" -> 2,
-    s"${v2.emit}.3" -> 3,
-    s"${v3.emit}.1.1" -> 311,
-    s"${v3.emit}.1.2" -> 312,
-    s"${v3.emit}.2.1" -> 321,
-    s"${v3.emit}.2.2" -> 322,
-    s"${v3.emit}.3.1" -> 331,
-    s"${v3.emit}.3.2" -> 332
+    TestScalaStan.v1.emit -> 0,
+    s"${TestScalaStan.v2.emit}.1" -> 1,
+    s"${TestScalaStan.v2.emit}.2" -> 2,
+    s"${TestScalaStan.v2.emit}.3" -> 3,
+    s"${TestScalaStan.v3.emit}.1.1" -> 311,
+    s"${TestScalaStan.v3.emit}.1.2" -> 312,
+    s"${TestScalaStan.v3.emit}.2.1" -> 321,
+    s"${TestScalaStan.v3.emit}.2.2" -> 322,
+    s"${TestScalaStan.v3.emit}.3.1" -> 331,
+    s"${TestScalaStan.v3.emit}.3.2" -> 332
   ).mapValues(_.toString)
 
   val results = StanResults(Vector(Vector(testData1, testData2)), model)
 
   describe("samples") {
     it("returns all scalar samples") {
-      results.samples(v1) shouldBe Seq(Seq(1, 0))
+      results.samples(TestScalaStan.v1) shouldBe Seq(Seq(1, 0))
     }
 
     it("returns all vector samples") {
-      results.samples(v2) shouldBe Seq(Seq(Seq(2.0, 3.0, 4.0), Seq(1.0, 2.0, 3.0)))
+      results.samples(TestScalaStan.v2) shouldBe Seq(Seq(Seq(2.0, 3.0, 4.0), Seq(1.0, 2.0, 3.0)))
     }
 
     it("returns all array of vector samples") {
-      results.samples(v3) shouldBe Seq(
+      results.samples(TestScalaStan.v3) shouldBe Seq(
         Seq(
           Seq(
             Seq(311, 312),
@@ -76,63 +78,63 @@ class StanResultsSpec extends ScalaStanBaseSpec {
 
   describe("best") {
     it("returns the best scalar sample") {
-      results.best(v1) shouldBe 0
+      results.best(TestScalaStan.v1) shouldBe 0
     }
   }
 
   describe("mean") {
     it("returns the mean scalar") {
-      results.mean(v1) shouldBe 0.5 // Note that this will always convert to double
+      results.mean(TestScalaStan.v1) shouldBe 0.5 // Note that this will always convert to double
     }
 
     it("returns the mean vector") {
-      results.mean(v2) shouldBe Vector(1.5, 2.5, 3.5)
+      results.mean(TestScalaStan.v2) shouldBe Vector(1.5, 2.5, 3.5)
     }
   }
 
   describe("variance") {
     it("returns the variance") {
-      results.variance(v1) shouldBe 0.5
+      results.variance(TestScalaStan.v1) shouldBe 0.5
     }
   }
 
   describe("sd") {
     it("returns the standard deviation") {
-      results.sd(v1) shouldBe math.sqrt(0.5)
+      results.sd(TestScalaStan.v1) shouldBe math.sqrt(0.5)
     }
   }
 
   describe("min") {
     it("returns the min") {
-      results.min(v1) shouldBe 0
+      results.min(TestScalaStan.v1) shouldBe 0
     }
   }
 
   describe("max") {
     it("returns the max") {
-      results.max(v1) shouldBe 1
+      results.max(TestScalaStan.v1) shouldBe 1
     }
   }
 
   describe("quantile") {
     it("returns 5%") {
-      results.quantile(v1, 0.05) shouldBe 0
+      results.quantile(TestScalaStan.v1, 0.05) shouldBe 0
     }
 
     it("returns 95%") {
-      results.quantile(v1, 0.95) shouldBe 1
+      results.quantile(TestScalaStan.v1, 0.95) shouldBe 1
     }
   }
 
   describe("effectiveSampleSize") {
     it("should return the effective sample size") {
-      results.effectiveSampleSize(v1) shouldBe 2
+      results.effectiveSampleSize(TestScalaStan.v1) shouldBe 2
     }
   }
 
   describe("mcse") {
     it("returns the mcse") {
-      results.mcse(v1) shouldBe 0.5
+      results.mcse(TestScalaStan.v1) shouldBe 0.5
     }
   }
 
@@ -144,7 +146,21 @@ class StanResultsSpec extends ScalaStanBaseSpec {
       pw.close()
       val summary = sw.toString.split("\n").toSeq
 
-      Seq("lp__", s"${v1.emit}", s"${v2.emit}", s"${v3.emit}").foreach { name =>
+      Seq("lp__", s"${TestScalaStan.v1.emit}", s"${TestScalaStan.v2.emit}", s"${TestScalaStan.v3.emit}").foreach { name =>
+        withClue(s"$name in $summary") {
+          summary.exists(_.startsWith(name)) shouldBe true
+        }
+      }
+    }
+
+    it("should filter") {
+      val sw = new StringWriter
+      val pw = new PrintWriter(sw)
+      results.summary(pw, TestScalaStan.v1)
+      pw.close()
+      val summary = sw.toString.split("\n").toSeq
+
+      Seq("lp__", s"${TestScalaStan.v1.emit}").foreach { name =>
         withClue(s"$name in $summary") {
           summary.exists(_.startsWith(name)) shouldBe true
         }

a3b2044717966cab642ea6936d36818beabf3b17
brendon- <brendon.puntin@gmail.com>
2017-12-12 20:49:59 -0600

    be explicit about what we are testing

--- src/it/scala/com/cibo/scalastan/examples/AppRunnerSpec.scala
+++ src/it/scala/com/cibo/scalastan/examples/AppRunnerSpec.scala
@@ -2,13 +2,20 @@ package com.cibo.scalastan.examples
 
 import org.scalatest.FunSpec
 
+import scala.util.{Failure, Success, Try}
+
 class AppRunnerSpec(app: App) extends FunSpec {
 
   private lazy val description = app.getClass.getSimpleName.stripSuffix("$")
 
   describe(description) {
     it("can run the app") {
-      app.main(Array.empty)
+      Try(app.main(Array.empty)) match {
+        case Success(_) => ()
+        case Failure(t) =>
+          t.printStackTrace()
+          fail(t)
+      }
     }
   }
 }

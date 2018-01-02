7838cdd5f04e343d16d0e870974ab4633b4df514
Joe Wingbermuehle <joewing@joewing.net>
2018-01-02 16:19:37 -0600

    Update headers.

--- src/main/scala/com/cibo/scalastan/transform/LoopChecker.scala
+++ src/main/scala/com/cibo/scalastan/transform/LoopChecker.scala
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2017 - 2018 CiBO Technologies - All Rights Reserved
+ * You may use, distribute, and modify this code under the
+ * terms of the BSD 3-Clause license.
+ *
+ * A copy of the license can be found on the root of this repository,
+ * at https://github.com/cibotech/ScalaStan/blob/master/LICENSE,
+ * or at https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package com.cibo.scalastan.transform
 
 import com.cibo.scalastan.ast._

172db8bd86710a8078c1c934b40fd892e132d797
Alex Klibisz <aklibisz@cibotechnologies.com>
2020-10-15 14:31:44 -0400

    just duplicate the STAN_VERSION env var

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-export STAN_VERSION=2.19.1
 export PATH="$PATH:$HOME/cmdstan-$STAN_VERSION/bin:$HOME/sbt/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(dirname $(dirname $(which clang)))/lib"
 export CMDSTAN_HOME="$HOME/cmdstan-$STAN_VERSION"
